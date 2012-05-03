14d5898f8ae6af66209385b5d09f73f0ddba4069
Alexander Wirt <alexander.wirt@credativ.de>
2012-05-03 17:08:51 +0200

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+class apt {
+	
+    "/etc/apt/apt.conf.d/local-recommends":
+      owner => root,
+      group => root,
+      source => "puppet:///modules/apt/etc/apt/apt.conf.d/local-recommends";
+
+    "/etc/apt/apt.conf.d/periodic-updates":
+      owner => root,
+      group => root,
+      source => "puppet:///modules/apt/etc/apt/apt.conf.d/periodic-updates";
+
+    "/etc/apt/sources.list.d/":
+      owner => root,
+      group => root,
+      source => "puppet:///modules/apt/etc/apt/apt.sources.list.d/backports.list";
+
+    #add backports to sources list
+
+}
