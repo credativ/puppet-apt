f6b6905757af27c8f10fb3a9188be1924aa80bde
Dennis Hoppe <dennis.hoppe@credativ.de>
2012-05-25 16:59:01 +0200

    Fixed several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,25 @@
 class apt {
-	
+  exec { 'apt-get update':
+    command     => 'apt-get update',
+    path        => '/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin',
+    refreshonly => true
+  }
+
   file {
-    "/etc/apt/apt.conf.d/local-recommends":
-      owner => root,
-      group => root,
-      source => "puppet:///modules/apt/etc/apt/apt.conf.d/local-recommends";
+    '/etc/apt/apt.conf.d/local-recommends':
+      owner  => 'root',
+      group  => 'root',
+      source => 'puppet:///modules/apt/etc/apt/apt.conf.d/local-recommends';
 
-    "/etc/apt/apt.conf.d/periodic-updates":
-      owner => root,
-      group => root,
-      source => "puppet:///modules/apt/etc/apt/apt.conf.d/periodic-updates";
+    '/etc/apt/apt.conf.d/periodic-updates':
+      owner  => 'root',
+      group  => 'root',
+      source => 'puppet:///modules/apt/etc/apt/apt.conf.d/periodic-updates';
 
-    "/etc/apt/sources.list.d/backports.list":
-      owner => root,
-      group => root,
-      notify  => Exec["apt-get update"],
-      source => "puppet:///modules/apt/etc/apt/sources.list.d/backports.list";
+    '/etc/apt/sources.list.d/backports.list':
+      owner  => 'root',
+      group  => 'root',
+      source => 'puppet:///modules/apt/etc/apt/sources.list.d/backports.list';
+      notify => Exec['apt-get update'],
   }
-
-  exec { "apt-get update":
-	  command => 'apt-get update',
-	  path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
-	  refreshonly => true
-  }
-
-
 }
